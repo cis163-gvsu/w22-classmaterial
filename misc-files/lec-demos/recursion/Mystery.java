8701c1f170bf240c8c6f8f4169ebf2538e0962d9
Erin Carrier <carrieer@gvsu.edu>
2022-02-09 14:16:50 -0500

    posts recursion material

---
+++ misc-files/lec-demos/recursion/Mystery.java
@@ -0,0 +1,23 @@
+public class Mystery {
+    
+    /* A mystery function named foo */
+    public int foo(int n) {
+        int res = 1;
+        for(int i=1; i<n; i++)
+            result = result * i;
+        return result
+    }
+
+    /*
+     * Q1. What is foo(0)?
+     * Q2. What is foo(1)?
+     * Q3. What is foo(2)?
+     * Q4. What is foo(4)?
+     *
+     * Can anyone detect the pattern?
+     * Given the pattern is there a different
+     *   way of implementing this?
+     * Is it better?
+     */
+
+}
